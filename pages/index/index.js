cad6683171ac2d82ac5ea02fb987483324c40c09
simon.du <a932455223@qq.com>
2017-01-07 23:51:41 +0800

    set the canvas height

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,22 +1,19 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-   
+    canvasHeight:0
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+    let that = this;
+    wx.getSystemInfo({
+    success: function(res) {
+    console.dir(res);
+    that.setData({
+      canvasHeight:res.windowWidth*2
+    });
+    
+  }
+})
+    
   }
 })
