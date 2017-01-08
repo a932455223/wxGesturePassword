cd36473c228b6796ed244dc12ad75bbf6b4634ad
simon.du <a932455223@qq.com>
2017-01-08 23:59:27 +0800

    some update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,68 @@
-var app = getApp()
+var app = getApp();
+
+var config = {
+        circle:{
+            sizeScale:1,
+            lineWidth:3,
+            default:{
+                strokeStyle:'#50A2E9'
+            },
+            wrong:{
+                strokeStyle:'#D90106'
+            },
+            right:{
+                strokeStyle:'#21864C'
+            }
+        },
+        line:{
+            lineWidth:3,
+            default:{
+                strokeStyle:'#50A2E9'
+            },
+            wrong:{
+                strokeStyle:'#D90106'
+            },
+            right:{
+                strokeStyle:'#21864C'
+            }
+        },
+        dot:{
+           size:8,
+            default:{
+                fillStyle:'#50A2E9'
+            },
+            wrong:{
+                fillStyle:'#D90106'
+            },
+            right:{
+                fillStyle:'#21864C'
+            }
+        }
+ };
+
+const ctx = wx.createCanvasContext('myCanvas');
+
+function rect(){
+   ctx.setFillStyle('red')
+    ctx.fillRect(10, 10, 150, 75)
+    ctx.draw()
+}
 Page({
   data: {
     canvasHeight:0
   },
   onLoad: function () {
     let that = this;
+
     wx.getSystemInfo({
     success: function(res) {
-    console.dir(res);
+      let winWidth = res.windowWidth*2;
     that.setData({
-      canvasHeight:res.windowWidth*2
+      canvasHeight:winWidth
     });
+
     
+
   }
 })
     
