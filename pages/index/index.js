0443bab864db648070780f9018bf35ccbd784745
simon.du <a932455223@qq.com>
2017-01-09 23:50:47 +0800

    draw a circle

--- pages/index/index.js
+++ pages/index/index.js
@@ -43,10 +43,11 @@ var config = {
 const ctx = wx.createCanvasContext('myCanvas');
 
 function rect(){
-   ctx.setFillStyle('red')
-    ctx.fillRect(10, 10, 150, 75)
-    ctx.draw()
+  ctx.setFillStyle('red')
+  ctx.fillRect(10, 10, 150, 75)
+  ctx.draw()
 }
+
 Page({
   data: {
     canvasHeight:0
@@ -56,13 +57,35 @@ Page({
 
     wx.getSystemInfo({
     success: function(res) {
-      let winWidth = res.windowWidth*2;
+      let winWidth = res.windowWidth;
     that.setData({
-      canvasHeight:winWidth
+      canvasHeight:winWidth*2
     });
-
-    
-
+    const count = [1,2,3];
+    let cr = winWidth/8 - config.circle.lineWidth;
+    let cm = (winWidth/8)*6;
+    count.forEach(function(row) {
+                count.forEach(function(column) {
+                    var x = (cr + config.circle.lineWidth + cm / 6) * (2 * column + 1);
+                    var y = (cr + config.circle.lineWidth) * (2 * row + 1) + 2 * row * cm / 6;
+                    // this.cList.push(new Point(x, y, 3 * row + column + 1));
+                    console.log("x:"+x,"y:"+y);
+                    // 
+                    // ctx.beginPath();
+                    // ctx.moveTo(x + cr, y);
+                    // ctx.arc(x, y, cr, 0, 2 * Math.PI);
+                    // ctx.setFillStyle('#EEEEEE')
+                    // ctx.fill();
+                }.bind(this));
+            }.bind(this));
+    // ctx.arc(cm/6+cr+config.circle.lineWidth, 75, cr, 0, 2 * Math.PI);
+    ctx.beginPath();
+                    // ctx.moveTo(x + cr, y);
+    ctx.arc(81.25, 34.375, cr, 0, 2 * Math.PI);
+    ctx.setFillStyle('#EEEEEE')
+    ctx.fill();
+    ctx.draw()
+      
   }
 })
     
