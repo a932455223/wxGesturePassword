735d7f261ef12be86880bea429fcb455d527df39
simon.du <a932455223@qq.com>
2017-01-11 00:25:33 +0800

    draw the circle

--- pages/index/index.js
+++ pages/index/index.js
@@ -61,29 +61,31 @@ Page({
     that.setData({
       canvasHeight:winWidth*2
     });
-    const count = [1,2,3];
+    const count = [0,1,2];
     let cr = winWidth/8 - config.circle.lineWidth;
-    let cm = (winWidth/8)*6;
+    console.log('cr:'+cr);
+    console.log('width:'+winWidth);
+    let cm = winWidth/(4*6);
     count.forEach(function(row) {
                 count.forEach(function(column) {
-                    var x = (cr + config.circle.lineWidth + cm / 6) * (2 * column + 1);
-                    var y = (cr + config.circle.lineWidth) * (2 * row + 1) + 2 * row * cm / 6;
+                    var x = (cr + config.circle.lineWidth + cm) * (2 * column + 1);
+                    var y = cr + config.circle.lineWidth+cm + 2*row*(cr+config.circle.lineWidth+cm);
                     // this.cList.push(new Point(x, y, 3 * row + column + 1));
                     console.log("x:"+x,"y:"+y);
                     // 
-                    // ctx.beginPath();
-                    // ctx.moveTo(x + cr, y);
-                    // ctx.arc(x, y, cr, 0, 2 * Math.PI);
-                    // ctx.setFillStyle('#EEEEEE')
-                    // ctx.fill();
+                    ctx.beginPath();
+                    ctx.moveTo(x + cr, y);
+                    ctx.arc(x, y, cr, 0, 2 * Math.PI);
+                    ctx.setFillStyle('#EEEEEE')
+                    ctx.fill();
                 }.bind(this));
             }.bind(this));
     // ctx.arc(cm/6+cr+config.circle.lineWidth, 75, cr, 0, 2 * Math.PI);
-    ctx.beginPath();
-                    // ctx.moveTo(x + cr, y);
-    ctx.arc(81.25, 34.375, cr, 0, 2 * Math.PI);
-    ctx.setFillStyle('#EEEEEE')
-    ctx.fill();
+    // ctx.beginPath();
+    //                 // ctx.moveTo(x + cr, y);
+    // ctx.arc(81.25, 84.375, cr, 0, 2 * Math.PI);
+    // ctx.setFillStyle('#EEEEEE')
+    // ctx.fill();
     ctx.draw()
       
   }
