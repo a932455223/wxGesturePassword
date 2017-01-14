316a8fa24fa3a54990455984b56c458946c7f1be
simon.du <a932455223@qq.com>
2017-01-14 20:32:05 +0800

    draw lines when touching

--- pages/index/index.js
+++ pages/index/index.js
@@ -43,15 +43,35 @@ var config = {
 
 
 const ctx = wx.createCanvasContext('myCanvas');
+
 let cList = [];
-function checkIn(x,y){
-    for(let p in cList){
-        
+let sList = [];
+let unList = [];
+let backImg;
+let cr;
+let cm;
+function checkIn(x,y,r){
+    for(let p of cList){
+
+        if(p.checked){
+            continue;
+        }
+        let dist = Math.sqrt(Math.pow((p.x-x),2) + Math.pow((p.y-y),2),2);
+        if(dist < r){
+            p.check();
+            sList.push(p);
+        }
     }
 }
-function Point(x,y){
+
+function Point(x,y,value){
     this.x = x;
     this.y = y;
+    this.value = value;
+    this.checked = false;
+    this.check = function(){
+        this.checked = true;
+    }
 }
 
 Page({
@@ -72,7 +92,6 @@ Page({
                 this.draw(winWidth);
             }
         })
-
     },
     start(e){
         if(this.data.touchId === false){
@@ -87,20 +106,20 @@ Page({
     },
     draw(winWidth){
         const count = [0, 1, 2];
-        let cr = winWidth / 8 - config.circle.lineWidth;
+        cr = winWidth / 8 - config.circle.lineWidth;
         console.log('cr:' + cr);
         console.log('width:' + winWidth);
-        let cm = winWidth / (4 * 6);
+        cm = winWidth / (4 * 6);
         count.forEach(function(row) {
             count.forEach(function(column) {
                 var x = (cr + config.circle.lineWidth + cm) * (2 * column + 1);
                 var y = cr + config.circle.lineWidth + cm + 2 * row * (cr + config.circle.lineWidth + cm);
-                cList.push(new Point(x,y));
+                cList.push(new Point(x,y,row*3+column));
                 ctx.beginPath();
                 ctx.moveTo(x + cr, y);
                 ctx.arc(x, y, cr, 0, 2 * Math.PI);
                 ctx.setLineWidth(config.circle.lineWidth);
-                ctx.setStrokeStyle('#EEEEEE')
+                ctx.setStrokeStyle(config.circle.default.strokeStyle);
                 ctx.stroke();
             });
         });
@@ -109,13 +128,50 @@ Page({
     move(e){
         let touch = e.changedTouches.filter((t)=>{
             return t.identifier === this.data.touchId;
-        })
+        })[0]
 
         if(touch){
             this.setData({
                 x: touch.x,
                 y: touch.y
             });
+
+            checkIn(touch.x,touch.y,this.data.canvasHeight/16);
+            ctx.clearRect(0,0,0,0);
+            cList.forEach(function(p,index){
+                let row = parseInt(index / 3);
+                let column = index % 3;
+                var x = (cr + config.circle.lineWidth + cm) * (2 * column + 1);
+                var y = cr + config.circle.lineWidth + cm + 2 * row * (cr + config.circle.lineWidth + cm);
+                ctx.beginPath();
+                ctx.moveTo(x + cr, y);
+                ctx.arc(x, y, cr, 0, 2 * Math.PI);
+                ctx.setLineWidth(config.circle.lineWidth);
+                ctx.setStrokeStyle(config.circle.default.strokeStyle);
+                ctx.stroke();
+                ctx.closePath();
+                if(p.checked){
+                    ctx.beginPath();
+                    ctx.setFillStyle(config.dot.default.fillStyle);
+                    ctx.arc(x,y,config.dot.size,0,2*Math.PI);
+                    ctx.fill();
+                    ctx.closePath();
+                }
+            });
+
+            if(sList.length > 1){
+                ctx.setStrokeStyle(config.line.default.strokeStyle);
+                ctx.setLineWidth(config.line.lineWidth);
+                for(let index = 1;index < sList.length;index++){
+                    let prePoint = sList[index - 1];
+                    let point = sList[index];
+                    ctx.moveTo(prePoint.x,prePoint.y);
+                    ctx.lineTo(point.x,point.y);
+                    ctx.stroke();
+                }
+            }
+
+            ctx.draw();
         }
 
     },
@@ -125,6 +181,7 @@ Page({
         })
 
         if(touch){
+
             this.setData({
                 touchId:false,
                 touching:false,
