c0dabc0ed33d6ddc64fc72f549433ce35a25d7e7
simon.du <a932455223@qq.com>
2017-01-15 22:29:19 +0800

    add state

--- pages/index/index.js
+++ pages/index/index.js
@@ -50,6 +50,7 @@ let unList = [];
 let backImg;
 let cr;
 let cm;
+let state = 'default';
 function checkIn(x,y,r){
     for(let p of cList){
 
@@ -74,6 +75,52 @@ function Point(x,y,value){
     }
 }
 
+function drawDotAndLine(){
+    cList.forEach(function(p,index){
+        let row = parseInt(index / 3);
+        let column = index % 3;
+        var x = (cr + config.circle.lineWidth + cm) * (2 * column + 1);
+        var y = cr + config.circle.lineWidth + cm + 2 * row * (cr + config.circle.lineWidth + cm);
+
+        ctx.beginPath();
+
+
+        ctx.moveTo(x + cr, y);
+        if(p.checked){
+            ctx.setStrokeStyle(config.circle[state].strokeStyle);
+        }else{
+            ctx.setStrokeStyle(config.circle.default.strokeStyle);
+        }
+        ctx.setLineWidth(config.circle.lineWidth);
+        ctx.arc(x, y, cr, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.closePath();
+        if(p.checked){
+            ctx.beginPath();
+            ctx.setFillStyle(config.dot[state].fillStyle);
+            ctx.arc(x,y,config.dot.size,0,2*Math.PI);
+            ctx.fill();
+            ctx.closePath();
+        }
+    });
+
+    if(sList.length > 1){
+        ctx.setStrokeStyle(config.line[state].strokeStyle);
+        ctx.setLineWidth(config.line.lineWidth);
+        ctx.beginPath();
+        for(let index = 1;index < sList.length;index++){
+            let prePoint = sList[index - 1];
+            let point = sList[index];
+            ctx.moveTo(prePoint.x,prePoint.y);
+            ctx.lineTo(point.x,point.y);
+            ctx.stroke();
+        }
+        ctx.closePath();
+    }
+
+    ctx.draw();
+}
+
 Page({
     data: {
         canvasHeight: 0,
@@ -94,6 +141,7 @@ Page({
         })
     },
     start(e){
+        console.dir(e);
         if(this.data.touchId === false){
             this.setData({
                 touching:true,
@@ -138,40 +186,7 @@ Page({
 
             checkIn(touch.x,touch.y,this.data.canvasHeight/16);
             ctx.clearRect(0,0,0,0);
-            cList.forEach(function(p,index){
-                let row = parseInt(index / 3);
-                let column = index % 3;
-                var x = (cr + config.circle.lineWidth + cm) * (2 * column + 1);
-                var y = cr + config.circle.lineWidth + cm + 2 * row * (cr + config.circle.lineWidth + cm);
-                ctx.beginPath();
-                ctx.moveTo(x + cr, y);
-                ctx.arc(x, y, cr, 0, 2 * Math.PI);
-                ctx.setLineWidth(config.circle.lineWidth);
-                ctx.setStrokeStyle(config.circle.default.strokeStyle);
-                ctx.stroke();
-                ctx.closePath();
-                if(p.checked){
-                    ctx.beginPath();
-                    ctx.setFillStyle(config.dot.default.fillStyle);
-                    ctx.arc(x,y,config.dot.size,0,2*Math.PI);
-                    ctx.fill();
-                    ctx.closePath();
-                }
-            });
-
-            if(sList.length > 1){
-                ctx.setStrokeStyle(config.line.default.strokeStyle);
-                ctx.setLineWidth(config.line.lineWidth);
-                for(let index = 1;index < sList.length;index++){
-                    let prePoint = sList[index - 1];
-                    let point = sList[index];
-                    ctx.moveTo(prePoint.x,prePoint.y);
-                    ctx.lineTo(point.x,point.y);
-                    ctx.stroke();
-                }
-            }
-
-            ctx.draw();
+            drawDotAndLine();
         }
 
     },
@@ -181,12 +196,15 @@ Page({
         })
 
         if(touch){
-
             this.setData({
                 touchId:false,
                 touching:false,
             });
         }
+        ctx.clearRect(0,0,this.data.canvasHeight,this.data.canvasHeight);
+        state = 'wrong';
+        drawDotAndLine();
+        console.dir(cList);
 
     }
 })
