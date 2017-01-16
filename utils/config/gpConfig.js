b4dacdb769be3caeb715bd60cdf9cb4f15302244
simon.du <a932455223@qq.com>
2017-01-16 23:33:21 +0800

    add background color and some other optimize

---
+++ utils/config/gpConfig.js
@@ -0,0 +1,41 @@
+var config = {
+    circle: {
+        sizeScale: 1,
+        lineWidth: 3,
+        default: {
+            strokeStyle: '#50A2E9'
+        },
+        wrong: {
+            strokeStyle: '#D90106'
+        },
+        right: {
+            strokeStyle: '#21864C'
+        }
+    },
+    line: {
+        lineWidth: 3,
+        default: {
+            strokeStyle: '#50A2E9'
+        },
+        wrong: {
+            strokeStyle: '#D90106'
+        },
+        right: {
+            strokeStyle: '#21864C'
+        }
+    },
+    dot: {
+        size: 8,
+        default: {
+            fillStyle: '#50A2E9'
+        },
+        wrong: {
+            fillStyle: '#D90106'
+        },
+        right: {
+            fillStyle: '#21864C'
+        }
+    }
+};
+
+module.exports = config;
